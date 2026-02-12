026d8adea88f8cca99dd4089f5e747c1c208a784
G2al <g2aluigi@gmail.com>
2026-02-12 22:12:22 +0100

    sistemato tante cose

--- app/Providers/Filament/ADMINPanelProvider.php
+++ app/Providers/Filament/ADMINPanelProvider.php
@@ -28,7 +28,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->canAccessPanel(fn () => true)
             ->colors([
                 'primary' => Color::Amber,
             ])
